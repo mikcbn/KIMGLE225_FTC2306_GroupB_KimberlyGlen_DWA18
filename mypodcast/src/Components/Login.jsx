4c8783a5c9039a3befa8fb41de758b9a123cc8f7
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-12-02 14:23:25 +0200

    Changed Favicon

--- mypodcast/src/Components/Login.jsx
+++ mypodcast/src/Components/Login.jsx
@@ -1,34 +1,14 @@
-import { createClient } from '@supabase/supabase-js';
+import React from 'react';
 import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { supabaseURL, supabaseAnonKey } from '../Clients/supabase'; // Import the environment variables
-
-const supabase = createClient(supabaseURL, supabaseAnonKey);
+import '@supabase/auth-ui/themes/supa.css'; // Import Supabase UI styles
 
 const Login = () => {
-  /** 
-   * Function to handle login using GitHub
-   */
-  const handleLogin = async () => {
-    try {
-      /** My attempt to sign in using Supabase authentication with GitHub provider  */
-      await supabase.auth.signIn({
-        provider: 'github',
-      });
-    } catch (error) {
-      /** Handle errors if any occur during the login process  */
-      console.error('Error logging in:', error.message);
-    }
-  };
-
-  /** Render the Login component  */
   return (
     <div>
       <h2>Login</h2>
-      <button onClick={handleLogin}>Login with GitHub</button>
+      <Auth supabaseClient={supabase} providers={['github']} />
     </div>
   );
 };
 
-/** Export the Login component as the default export  */
 export default Login;
